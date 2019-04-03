d8b2554cf5f58d1dfbc1f7c6645103f21c99b49a
Aurimas <aurimas@darguzis.com>
2019-04-03 16:43:08 +0100

    building header

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
+import Toolbar from '../navigation/Toolbar/Toolbar';
 
 export default function Layout(props) {
   return (
     <Aux>
-      <div>Toolbar, SideDrawer, Backdrop</div>
+      <Toolbar>{props.children}</Toolbar>
       <main className={classes.Content}>{props.children}</main>
     </Aux>
   );

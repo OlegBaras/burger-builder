3d3d1fe537cbe086c7c05da1cad6cb3ac0efbd05
aurimas-darguzis <aurimas@darguzis.com>
2019-04-03 23:23:06 +0100

    improvement(order-summary): don't render until modal is visible

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
-import Button from '../../UI/Button/Button';
+import React, { Component } from "react";
+import Aux from "../../../hoc/Aux";
+import Button from "../../UI/Button/Button";
 
-export default function OrderSummary(props) {
-  const ingredientSummary = Object.keys(props.ingredients).map(i => {
+export default class OrderSummary extends Component {
+  // This cuold be a functional component. Only for demo purpose the life cycle is set up here
+  componentWillUpdate() {
+    console.log("[OrderSummary] Willupdate");
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(i => {
+      return (
+        <li key={i}>
+          <span style={{ textTransform: "capitalize" }}>{i}</span>:
+          {this.props.ingredients[i]}
+        </li>
+      );
+    });
     return (
-      <li key={i}>
-        <span style={{ textTransform: 'capitalize' }}>{i}</span>:
-        {props.ingredients[i]}
-      </li>
-    );
-  });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>
-          Total Price: {props.price.toFixed(2)} {props.currency}
-        </strong>
-      </p>
-      <p>Continue to checkout</p>
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>
+            Total Price: {this.props.price.toFixed(2)} {this.props.currency}
+          </strong>
+        </p>
+        <p>Continue to checkout</p>
 
-      <Button btnType='Danger' clicked={props.purchaseCanceled}>
-        CANCEL
-      </Button>
-      <Button btnType='Success' clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
+        <Button btnType="Danger" clicked={this.props.purchaseCanceled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
 }

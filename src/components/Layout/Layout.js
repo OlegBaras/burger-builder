1e774811cc6e607b7deab09eb7af0ac013b337ad
Aurimas <aurimas@darguzis.com>
2019-04-03 20:57:13 +0100

    fix(layout): side drawer default to false

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ import SideDrawer from '../navigation/SideDrawer/SideDrawer';
 
 export default class Layout extends Component {
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   };
 
   sideDrawerClosedHandler = () => {

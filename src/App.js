272e41c8f9fb417f448f58293269ffc2774a7e72
Aurimas <aurimas@darguzis.com>
2019-05-14 23:34:34 +0100

    converted app to functional c

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -19,37 +19,36 @@ const asyncAuth = asyncComponent(() => {
   return import('./containers/Auth/Auth.js');
 });
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onTryAutoSignup();
-  }
-  render() {
-    let routes = (
+const app = props => {
+  useEffect(() => {
+    props.onTryAutoSignup();
+  }, []);
+
+  let routes = (
+    <Switch>
+      <Route path='/auth' component={asyncAuth} />
+      <Route path='/' exact component={BurgerBuilder} />
+      <Redirect to='/' />
+    </Switch>
+  );
+
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
+        <Route path='/checkout' component={asyncCheckout} />
+        <Route path='/orders' component={asyncOrders} />
         <Route path='/auth' component={asyncAuth} />
+        <Route path='/logout' component={Logout} />
         <Route path='/' exact component={BurgerBuilder} />
-        <Redirect to='/' />
       </Switch>
     );
-
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path='/checkout' component={asyncCheckout} />
-          <Route path='/orders' component={asyncOrders} />
-          <Route path='/auth' component={asyncAuth} />
-          <Route path='/logout' component={Logout} />
-          <Route path='/' exact component={BurgerBuilder} />
-        </Switch>
-      );
-    }
-    return (
-      <div>
-        <Layout>{routes}</Layout>
-      </div>
-    );
   }
-}
+  return (
+    <div>
+      <Layout>{routes}</Layout>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return {
@@ -67,5 +66,5 @@ export default withRouter(
   connect(
     mapStateToProps,
     mapDispatchToProps
-  )(App)
+  )(app)
 );
